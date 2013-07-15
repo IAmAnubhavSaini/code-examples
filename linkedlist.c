15aa9aae42ab399a8a66a0b4cfa27fc5a0dc89f2
anubhav saini <iamanubhavsaini@gmail.com>
2013-07-15 18:41:14 +0530

    debugged and refined. Working Now.

--- linkedlist.c
+++ linkedlist.c
@@ -1,66 +1,68 @@
-#include&lt;stdio.h&gt;
-#include&lt;stdlib.h&gt;
-#include&lt;stdbool.h&gt;
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
 
 struct test_struct
 {
-    int val;
-    struct test_struct *addr;
+  int val;
+  struct test_struct *next;
 };
+typedef struct test_struct TestStruct; //gives a bit of OOP feel
 
-struct test_struct *head = NULL;
-struct test_struct *curr = NULL;
+TestStruct *head = NULL;
+TestStruct *curr = NULL;
 
-struct test_struct* create_list(int val){
-    struct test_struct *ptr = (struct test_struct*)malloc(sizeof(struct test_struct));
-    if(NULL == ptr){
-        printf("Cannot create a node");
-        return NULL;
-    }
-    
-    ptr-&gt;val = val;
-    ptr-&gt;addr = NULL;
-    
-    head = curr = ptr;
-    return ptr;
+TestStruct* create_list(int val){
+  TestStruct *ptr = (TestStruct*)malloc(sizeof(TestStruct));
+  if(ptr == NULL){
+    printf("\n*Cannot create a node.*\n");
+    return NULL;
+  }
+
+  ptr->val = val;
+  ptr->next = NULL;
+
+  head = curr = ptr;
+  return ptr;
 }
 
-struct test_struct* add_to_list(int val,bool before_head){
-    struct test_struct *ptr = (struct  test_struct*)malloc(sizeof(struct test_struct));
-    if(NULL == head){
-        return(create_list(val));
-    }
-    
-    if(!before_head) {
-        ptr-&gt;val = val;
-        curr-&gt;addr = ptr;
-        curr = ptr;
-        return ptr;
-    }
-    else{
-        ptr-&gt;val = val;
-        ptr-&gt;addr = head;
-        head = ptr;
-        return ptr;
-    }
+TestStruct* add_to_list(int val, bool before_head){
+  TestStruct *newTS = (TestStruct*)malloc(sizeof(TestStruct));
+  if(head== NULL){
+    return(create_list(val));
+  }
+
+  if(before_head) {
+    newTS->val = val;
+    newTS->next = head;
+    head = newTS;
+    return head;//try returning head so that further manipulation can start from right there.
+  }
+  else{
+    newTS->val = val;
+    newTS->next = NULL;
+    curr->next = newTS;
+    curr = newTS;
+    return curr;//try returning current so that further manipulation can start from right there.
+  }
 }
 
 void print_list(){
-    
-    struct test_struct *ptr = head;
-    printf("\nThe values of the list are\n");
-    while(ptr){
-        printf("\t%d\t",ptr-&gt;val);
-        ptr = ptr-&gt;addr;
-    }
-    
+
+  TestStruct *ptr = head;
+  printf("\nThe values of the list are\n");
+  while(ptr){
+    printf("\t%d\t",ptr->val);
+    ptr = ptr->next;
+  }
+
 }
 
 void main(){
-    struct test_struct *ptr = NULL;
-    add_to_list(1,false);
-    add_to_list(2,false);
-    add_to_list(3,false);
-    add_to_list(8,true);
-    print_list();
+  TestStruct *ptr = NULL;
+  add_to_list(1,false);
+  add_to_list(2,false);
+  add_to_list(3,false);
+  add_to_list(8,true);
+  print_list();
 }
