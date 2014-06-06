6188fd4b27a524e95f179045558bb9a026afad6b
Anubhav Saini <IAmAnubhavSaini@GMail.com>
2014-06-06 12:07:00 +0530

    Just some tweaks.

--- src/series/1_if_integer_odd_or_even.c
+++ src/series/1_if_integer_odd_or_even.c
@@ -1,16 +1,16 @@
 #include "common.h"
 
-BOOL IsIntOdd(int toCheck)
+BOOL IsIntOdd ( int toCheck )
 {
-    if(toCheck % 2 != 0) 
+    if ( toCheck % 2 != 0 ) 
         return TRUE; // yes, toCheck is odd
     else
         return FALSE; // no, toCheck is even!
 }
 
-BOOL IsIntOdd_Alt(int toCheck)
+BOOL IsIntOdd_Alt ( int toCheck )
 {
-    if(toCheck & 1 == 1) 
+    if ( toCheck & 1 == 1 ) 
         return TRUE; // yes, toCheck is odd
     else
         return FALSE; // no, toCheck is even!
--- src/series/2_if_integer_is_negative.c
+++ src/series/2_if_integer_is_negative.c
@@ -1,7 +1,9 @@
 #include "common.h"
 
-BOOL IsIntNegative(int toCheck)
+BOOL IsIntNegative ( int toCheck )
 {
-    if(toCheck < 0) return TRUE; // yes
-    else            return FALSE; // no
+    if ( toCheck < 0 ) 
+        return TRUE; // yes
+    else
+        return FALSE; // no
 }
