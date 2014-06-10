17451d5dbff18468fb0b33de7805a691b05f3e8a
Anubhav Saini <IAmAnubhavSaini@GMail.com>
2014-06-10 18:52:55 +0530

    4. finding sum, diff and trace of matrices

--- src/series/a lot of code/4_add_sub_trace_matrix.c
+++ src/series/a lot of code/4_add_sub_trace_matrix.c
@@ -10,23 +10,40 @@
 
 void AddIntMatrices ( int A[ROWS][COLS], int B[ROWS][COLS], int Sum[ROWS][COLS], int m, int n )
 {
-    int i = 0, j = 0;
-    for( i = 0; i < m; ++i ){ // m is taken to be rows
-        for ( j = 0; j < n; ++j ){ // n is taken to be columns
-            *(*(Sum+i)+j) = A[i][j] + B[i][j];
+        int i = 0, j = 0;
+        for( i = 0; i < m; ++i ){ // m is taken to be rows
+            for ( j = 0; j < n; ++j ){ // n is taken to be columns
+                *(*(Sum+i)+j) = A[i][j] + B[i][j];
+            }
         }
-    }
 }
 
 void SubIntMatrices ( int A[ROWS][COLS], int B[ROWS][COLS], int Diff[ROWS][COLS], int m, int n )
 /* remember, subtraction has peculiarity A-B != B-A */
 {
-    int i = 0, j = 0;
-    for( i = 0; i < m; ++i ){ // m is taken to be rows
-        for ( j = 0; j < n; ++j ){ // n is taken to be columns
-            *(*(Diff+i)+j) = A[i][j] - B[i][j];
+        int i = 0, j = 0;
+        for( i = 0; i < m; ++i ){ // m is taken to be rows
+            for ( j = 0; j < n; ++j ){ // n is taken to be columns
+                *(*(Diff+i)+j) = A[i][j] - B[i][j];
+            }
         }
-    }
+}
+
+int TraceIntMatrix ( int A[ROWS][COLS], m, n )
+{
+        int trace = 0;
+        int i = 0, j = 0;
+        for ( i = 0; i < m; ++i ) {
+                trace += A[i][i];
+        }
+        return trace;
+}
+
+BOOL IsIntMatricesTraceEqual ( int A[ROWS][COLS], int B[ROWS][COLS], int Diff[ROWS][COLS], int m, int n )
+{
+        int traceA = TraceIntMatrix ( A, m, n );
+        int traceB = TraceIntMatrix ( B, m, n );
+        return traceA == traceB;
 }
 
 
---
+++ src/series/case studies in c/singly_linked_list_library.h
@@ -0,0 +1,12 @@
+#ifndef CASE_STUDIES_SLL_COMMON_H
+#define CASE_STUDIES_SLL_COMMON_H
+#include <stdio.h>
+#include <stdlib.h>
+
+struct sllnode {
+        struct sllnode * next;
+        int data;
+};
+
+
+#endif
